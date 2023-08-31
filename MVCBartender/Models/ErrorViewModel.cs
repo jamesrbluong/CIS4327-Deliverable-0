e06456fad5229b188f8c6d20074652e197ff1882
frostyfusion <128750662+jamesrbluong@users.noreply.github.com>
2023-08-31 16:22:53 -0400

    Initial Commit
    First initial WIP upload of Deliverable 0 - MVC Bartender Project -- uses local database

---
+++ MVCBartender/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCBartender.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
